2109df85f436971fd32de052faf04a7acd9a2397
chaithanyakrishnasn <chaithanyakrishnasn@gmail.com>
2026-03-05 23:00:51 +0530

    update action factory

--- app/actions/factory.py
+++ app/actions/factory.py
@@ -1,11 +1,12 @@
 from app.actions.log_action import LogAction
+from app.core.enums import ActionType
 
 
 class ActionFactory:
 
     @staticmethod
     def get_action(action_type: str, db):
-        if action_type.lower() == "log":
+        if action_type == ActionType.LOG:
             return LogAction(db)
 
         raise ValueError(f"Unsupported action type: {action_type}")
