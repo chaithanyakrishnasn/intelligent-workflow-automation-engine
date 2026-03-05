bbd7b6c1376d262823ad19404e26e80d725dbb8d
incharareddy <lakshmiincharam@gmail.com>
2026-03-05 22:58:49 +0530

    update action action schema

--- app/schemas/action.py
+++ app/schemas/action.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel
 from typing import Optional
+from app.core.enums import ActionType
 
 
 class ActionCreate(BaseModel):
-    type: str
+    type: ActionType
     config: Optional[str] = None
 
 
 class ActionResponse(BaseModel):
     id: int
-    type: str
+    type: ActionType
     config: Optional[str]
     workflow_id: int
 
