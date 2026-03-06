31dc8799b9a203311603b738e4394c4cb490f61d
chaithanyakrishnasn <chaithanyakrishnasn@gmail.com>
2026-03-06 21:47:00 +0530

    update workflow

--- app/services/workflow_service.py
+++ app/services/workflow_service.py
@@ -3,6 +3,9 @@ from app.models.workflow import Workflow
 from app.core.scheduler import scheduler
 from app.services.execution_service import ExecutionService
 from app.core.scheduler import remove_job
+from app.models.trigger import Trigger
+from app.core.scheduler import scheduler
+from app.services.execution_service import ExecutionService
 
 
 class WorkflowService:
@@ -81,6 +84,7 @@ def deactivate_workflow(db, workflow_id: int):
 
 
 def activate_workflow(db, workflow_id: int):
+
     workflow = db.query(Workflow).filter(Workflow.id == workflow_id).first()
 
     if not workflow:
@@ -88,5 +92,22 @@ def activate_workflow(db, workflow_id: int):
 
     workflow.is_active = True
     db.commit()
+
+    trigger = db.query(Trigger).filter(
+        Trigger.workflow_id == workflow_id
+    ).first()
+
+    if trigger and trigger.type == "timer":
+
+        interval = int(trigger.config)
+
+        scheduler.add_job(
+            ExecutionService.execute_workflow,
+            "interval",
+            seconds=interval,
+            args=[db, workflow_id],
+            id=f"workflow_{workflow_id}",
+            replace_existing=True
+        )
 
     return workflow
